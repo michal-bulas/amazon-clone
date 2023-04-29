b696c6451b434fa1030d233c042f93b7edc0ccd3
michal-bulas <michal.bulas1@gmail.com>
2023-04-29 09:09:46 +0200

    add more cart content

--- firebase.ts
+++ firebase.ts
@@ -1,4 +1,4 @@
-import { firebase } from 'firebase';
+import firebase from 'firebase';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyDFQU6N2y0WIwjHtug4a1_hFEEt3dB1cc8',
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,11 +1,13 @@
 import CheckoutProduct from '@/components/CheckoutProduct';
 import Header from '@/components/Header';
 import { selectItems } from '@/store/slices/cartSlice';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
 
 const Checkout = () => {
 	const items = useSelector(selectItems);
+	const { data: session } = useSession();
 
 	return (
 		<div className='bg-gray-100'>
@@ -17,9 +19,9 @@ const Checkout = () => {
 						height={250}
 						src='/bannerCheckout.jpg'
 						alt='Checkout Image'
-						className='object-contain'
+						className='object-contain w-full'
 					/>
-					<div className='flex flex-col p-5 space-y-10 bg-white'>
+					<div className='flex flex-col p-5 space-y-10 bg-white shadow-md'>
 						<h1 className='text-3xl border-b pb-4'>
 							{items.length === 0 ? 'Your Cart is Empty' : 'Your Shopping Cart'}
 						</h1>
@@ -31,6 +33,23 @@ const Checkout = () => {
 						))}
 					</div>
 				</div>
+				{items.length > 0 && (
+					<div className='flex flex-col bg-white p-10 lg:mb-5 shadow-md'>
+						<h2 className='whitespace-nowrap'>
+							Subtotal ({items.length} items):
+							<span></span>
+						</h2>
+						<button
+							disabled={!session}
+							className={`button mt-2 ${
+								!session &&
+								'from-gray-300 to gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+							}`}
+						>
+							{!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+						</button>
+					</div>
+				)}
 			</main>
 		</div>
 	);
