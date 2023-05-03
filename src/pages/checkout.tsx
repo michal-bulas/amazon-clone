00e4f521aff8430da3cf20e95bfbf0d4f5a7b6e7
michal-bulas <michal.bulas1@gmail.com>
2023-05-04 00:57:09 +0200

    fix err

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -41,8 +41,22 @@ const Checkout = () => {
 				email: session.user.email,
 			})
 			.catch(function (error) {
-				// Handle the error
-				console.error('Error during the checkout session:', error);
+				if (error.response) {
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.log('Error response data:', error.response.data);
+					console.log('Error response status:', error.response.status);
+					console.log('Error response headers:', error.response.headers);
+				} else if (error.request) {
+					// The request was made but no response was received
+					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+					// http.ClientRequest in node.js
+					console.log('Error request:', error.request);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log('Error message:', error.message);
+				}
+				console.log('Error config:', error.config);
 
 				// Return an object with a data property to avoid type errors
 				return { data: { id: null } };
