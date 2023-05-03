1ee1dccb297d4fe388e122b3173db16f2fd67dd7
michal-bulas <michal.bulas1@gmail.com>
2023-05-04 01:18:17 +0200

    move head tag to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,28 @@ import store from '@/store/store';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<SessionProvider session={pageProps.session}>
 			<Provider store={store}>
+				<Head>
+					<title>Amazon Clone</title>
+					<meta
+						name='description'
+						content='Amazon Clone Project'
+					/>
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1'
+					/>
+					<link
+						rel='icon'
+						href='/amazon.ico'
+						type='image/x-icon'
+					/>
+				</Head>
 				<Component {...pageProps} />
 			</Provider>
 		</SessionProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,13 @@ import ProductFeed from '@/components/ProductFeed';
 import { ProductProps } from '@/types/ProductProps';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
-import Head from 'next/head';
 
 const Home: React.FC<{ products: ProductProps[] }> = ({ products }) => {
 	return (
 		<div className='bg-gray-100'>
-			<Head>
-				<title>Amazon Clone</title>
-				<meta
-					name='description'
-					content='Amazon Clone Project'
-				/>
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1'
-				/>
-				<link
-					rel='icon'
-					href='/amazon.ico'
-					type='image/x-icon'
-				/>
-			</Head>
 			<Header />
 			<main className='max-w-screen-2xl mx-auto'>
 				<Banner />
-
 				<ProductFeed products={products} />
 			</main>
 		</div>
