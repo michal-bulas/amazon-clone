674a1370e652dce87a8f9825c86a37840c838025
michal-bulas <michal.bulas1@gmail.com>
2023-05-04 00:50:45 +0200

    fix err

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -35,10 +35,29 @@ const Checkout = () => {
 			return;
 		}
 
-		const checkoutSession = await axios.post('/api/checkout-session', {
-			items: items,
-			email: session.user.email,
-		});
+		const checkoutSession = await axios
+			.post('/api/checkout-session', {
+				items: items,
+				email: session.user.email,
+			})
+			.catch(function (error) {
+				if (error.response) {
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				} else if (error.request) {
+					// The request was made but no response was received
+					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+					// http.ClientRequest in node.js
+					console.log(error.request);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log('Error', error.message);
+				}
+				console.log(error.config);
+			});
 
 		const result = await stripe.redirectToCheckout({
 			sessionId: checkoutSession.data.id,
